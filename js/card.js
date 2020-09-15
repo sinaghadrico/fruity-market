a77ad59bbabb99ead8ff376be5fc6e0a56d87510
Ted NIVAN <ted.jp.nivan@gmail.com>
2020-09-15 16:03:38 +0200

    First commit

---
+++ js/card.js
@@ -0,0 +1,147 @@
+let cardsData = [{
+    id: 0,
+    name: 'Strawberry',
+    image: 'assets/png/028-strawberry.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 1,
+    name: 'Banana',
+    image: 'assets/png/006-bananas.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 2,
+    name: 'Pear',
+    image: 'assets/png/024-pear.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 3,
+    name: 'Dragon Fruit',
+    image: 'assets/png/015-dragon fruit.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 4,
+    name: 'Orange',
+    image: 'assets/png/022-orange.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 5,
+    name: 'Mango',
+    image: 'assets/png/021-mango.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 6,
+    name: 'Lemon',
+    image: 'assets/png/020-lemon.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 7,
+    name: 'Watermelon',
+    image: 'assets/png/030-watermelon.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 8,
+    name: 'Berry',
+    image: 'assets/png/008-berry.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 9,
+    name: 'Pomegranate',
+    image: 'assets/png/025-pomegranate.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 10,
+    name: 'Blueberry',
+    image: 'assets/png/009-blueberry.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  {
+    id: 11,
+    name: 'Rose Apple',
+    image: 'assets/png/027-rose apple.png',
+    avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+    user: {
+      name: 'Okinami',
+      handle: 'twitterid',
+      title: 'Lead Developer'
+    },
+    content: 'The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken, trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for it had become hideous and twisted.'
+  },
+  ]
+  
+ 
